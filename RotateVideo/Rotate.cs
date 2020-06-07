2f939f81e51e9ea6afeb40099b34326afc114253
Jogchem Andre Oord <jogchem.oord@helloclarity.nl>
2020-06-07 23:02:26 +0200

    - make letterbox instead of rotate - make sure input and output paths exist - if no path is given in the input file, use current directory

--- RotateVideo/Rotate.cs
+++ RotateVideo/Rotate.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.ExceptionServices;
 using System.Text;
 
 namespace RotateVideo
@@ -17,32 +18,29 @@ namespace RotateVideo
             _config = config;
         }
 
-
         public void Do(int currentRotation, string mediaFile)
         {
-            string ffmpeg = _config.GetSection("ffmpeg").Value;
             var outputPath = GetOutputPath(mediaFile);
 
-
-            string transpose = string.Empty;
-
-            if (currentRotation == 0)
-            {
-                // do nothing, move to output path
-                File.Copy(mediaFile, outputPath);
+            if (!File.Exists(mediaFile)) { 
+                Console.WriteLine("Input file does not exist");
                 return;
             }
-            else //if (currentRotation == 90)
+
+            if(!Directory.Exists(outputPath))
             {
-                transpose = "transpose=3,transpose=3";
+                Console.WriteLine($"Output path {outputPath} does not exist");
+                return;
             }
 
-            string arguments = $"-i {mediaFile} -metadata:s:v:0 rotate=0 -vf \"{transpose}\" \"{outputPath}\"";
-            var ffMpeg = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _config.GetSection("ffmpeg").Value);
+            string outputFile = Path.Combine(outputPath, Path.GetFileName(mediaFile));
+
+            var letterBox = $"-vf \"scale=(iw*sar)*min(1280/(iw*sar)\\,720/ih):ih*min(1280/(iw*sar)\\,720/ih), pad=1280:720:(1280-iw*min(1280/iw\\,720/ih))/2:(720-ih*min(1280/iw\\,720/ih))/2\"";
+            string arguments = $"-i {mediaFile} {letterBox} \"{outputFile}\"";
 
             using (var process = new Process())
             {
-                process.StartInfo.FileName = ffMpeg;
+                process.StartInfo.FileName = _config.GetSection("ffmpeg").Value;
                 process.StartInfo.Arguments = arguments;
 
                 process.StartInfo.CreateNoWindow = true;
@@ -51,17 +49,20 @@ namespace RotateVideo
                 process.Start();
                 process.WaitForExit(100);
             }
-
             Console.WriteLine($"File exported to {outputPath}");
         }
-
 
         private string GetOutputPath(string mediaFile)
         {
             string cfgOutputPath = _config.GetSection("outputpath").Value;
             string inputPathWithoutFile = Path.GetDirectoryName(mediaFile);
+            
+            if(string.IsNullOrEmpty(inputPathWithoutFile))
+            {
+                inputPathWithoutFile = Environment.CurrentDirectory;
+            }
 
-            return Path.Combine(cfgOutputPath.Replace("{inputpath}", inputPathWithoutFile), Path.GetFileName(mediaFile));
+            return cfgOutputPath.Replace("{inputpath}", inputPathWithoutFile);
         }
 
     }
